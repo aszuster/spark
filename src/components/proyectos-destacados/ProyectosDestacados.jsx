19d9bc549b9f929143040887b4a422e74901f8f4
Alejandra <ale.szuster@gmail.com>
2025-10-08 18:32:45 -0300

    proyectos destacados mobile

--- src/components/proyectos-destacados/ProyectosDestacados.jsx
+++ src/components/proyectos-destacados/ProyectosDestacados.jsx
@@ -10,7 +10,10 @@ import Chevron from "../../svg/Chevron";
 
 const ProyectosDestacados = () => {
   return (
-    <section id="proyectos-destacados" className="hidden lg:block lg:h-screen w-full relative bg-secondary-200">
+    <section
+      id="proyectos-destacados"
+      className="lg:h-screen w-full relative bg-secondary-200"
+    >
       <img
         src="/img/proyectos-destacados/k.svg"
         alt=""
@@ -30,7 +33,7 @@ const ProyectosDestacados = () => {
           <ArrowButton
             text="Antecedentes (.pdf)"
             download={true}
-            className="text-p-blue-500! bg-gradient-to-br! from-[#E2E5EA]! to-[#EFF1F6]! border-[1px]! border-secondary-000!"
+            className="text-p-blue-500! bg-gradient-to-br! from-[#E2E5EA]! to-[#EFF1F6]! border-[1px]! border-secondary-000! w-full justify-between lg:w-fit lg:justify-start"
           />
         </div>
       </div>
@@ -50,28 +53,64 @@ const ProyectosDestacados = () => {
               centeredSlides: true,
             },
             1920: {
-              slidesPerView: 'auto',
+              slidesPerView: "auto",
               spaceBetween: 150,
               centeredSlides: true,
             },
           }}
           modules={[Navigation, Pagination]}
           className="mySwiper mb-8 lg:mb-0"
-          navigation={{ 
-            nextEl: ".arrow-right, .arrow-right-mobile", 
-            prevEl: ".arrow-left, .arrow-left-mobile" 
+          navigation={{
+            nextEl: ".arrow-right, .arrow-right-mobile",
+            prevEl: ".arrow-left, .arrow-left-mobile",
           }}
-          pagination={{ 
+          pagination={{
             clickable: true,
-            el: '.custom-pagination-proyectos',
+            el: ".custom-pagination-proyectos",
             renderBullet: function (index, className) {
               return '<div class="' + className + '"></div>';
             },
           }}
         >
           <SwiperSlide>
-            <div className="w-full lg:w-[918px] h-[462px] bg-[url('/img/proyectos-destacados/01.webp')] bg-cover bg-center rounded-[20px] lg:rounded-[40px] px-[20px] lg:px-[30px] py-[30px] lg:py-[40px] flex items-end justify-end">
-              <div className="px-[16px] lg:px-[20px] py-[20px] lg:py-[24px] bg-secondary-000 rounded-[16px] lg:rounded-[20px] w-full lg:w-[464px] h-auto">
+            <div className="flex flex-col w-full h-full mx-[20px] lg:mx-0 md:block md:mx-0">
+              <div className="w-full h-[280px] lg:w-[918px] lg:h-[462px] bg-[url('/img/proyectos-destacados/01.webp')] bg-cover bg-center rounded-[20px] lg:rounded-[40px] px-[20px] lg:px-[30px] py-[30px] lg:py-[40px] flex items-end justify-end">
+                <div className="hidden md:block px-[16px] lg:px-[20px] py-[20px] lg:py-[24px] bg-secondary-000 rounded-[16px] lg:rounded-[20px] w-full lg:w-[464px] h-auto">
+                  <div className="flex items-center justify-between">
+                    <div className="] bg-p-orange-600 rounded-[6px] h-[26px] w-[70px] flex items-center justify-center">
+                      <span className="text-secondary-000 font-inter text-[14px] leading-[14px] tracking-[-0.7px] uppercase">
+                        Cliente
+                      </span>
+                    </div>
+                    <div className="w-[106px] h-auto">
+                      <img
+                        src="/img/clientes/pan-american.svg"
+                        alt=""
+                        className="w-[106px] h-auto"
+                      />
+                    </div>
+                  </div>
+                  <div className="flex items-center gap-[8px]">
+                    {/* <Chevron stroke="#4A5468" className="w-[6px] h-[14px]" /> */}
+                    <p className="text-secondary-700 font-inter font-semibold text-[16px] leading-[24px] tracking-[-0.64px]">
+                      Proyecto:
+                    </p>
+                  </div>
+                  <p className="text-p-blue-600 font-muli text-[14px] leading-[24px] tracking-[-0.56px] mb-[10px]">
+                    PO Planta Compresora
+                  </p>
+                  <div className="flex items-center gap-[8px]">
+                    {/* <Chevron stroke="#4A5468" className="w-[6px] h-[14px]" /> */}
+                    <p className="text-secondary-700 font-inter font-semibold text-[16px] leading-[24px] tracking-[-0.64px]">
+                      Alcance:
+                    </p>
+                  </div>
+                  <p className="text-p-blue-600 font-muli text-[14px] leading-[24px] tracking-[-0.56px]">
+                    Ingeniería Conceptual y Básica
+                  </p>
+                </div>
+              </div>
+              <div className="block md:hidden bg-secondary-000 rounded-[16px]  w-full  h-full px-[20px] py-[24px]">
                 <div className="flex items-center justify-between">
                   <div className="] bg-p-orange-600 rounded-[6px] h-[26px] w-[70px] flex items-center justify-center">
                     <span className="text-secondary-000 font-inter text-[14px] leading-[14px] tracking-[-0.7px] uppercase">
@@ -108,8 +147,9 @@ const ProyectosDestacados = () => {
             </div>
           </SwiperSlide>
           <SwiperSlide>
-            <div className="w-full lg:w-[918px] h-[462px] bg-[url('/img/proyectos-destacados/02.webp')] bg-cover bg-bottom rounded-[20px] lg:rounded-[40px] px-[20px] lg:px-[30px] py-[30px] lg:py-[40px] flex items-end justify-end">
-              <div className="px-[16px] lg:px-[20px] py-[20px] lg:py-[24px] bg-secondary-000 rounded-[16px] lg:rounded-[20px] w-full lg:w-[464px] h-auto">
+            <div className="flex flex-col w-full h-full mx-[20px] lg:mx-0 md:block md:mx-0">
+            <div className="w-full h-[280px] lg:mx-0 lg:w-[918px] md:h-[462px] bg-[url('/img/proyectos-destacados/02.webp')] bg-cover bg-bottom rounded-[20px] lg:rounded-[40px] px-[20px] lg:px-[30px] py-[30px] lg:py-[40px] flex items-end justify-end">
+              <div className="hidden md:block px-[16px] lg:px-[20px] py-[20px] lg:py-[24px] bg-secondary-000 rounded-[16px] lg:rounded-[20px] w-full lg:w-[464px] h-auto">
                 <div className="flex items-center justify-between">
                   <div className="] bg-p-orange-600 rounded-[6px] h-[26px] w-[70px] flex items-center justify-center">
                     <span className="text-secondary-000 font-inter text-[14px] leading-[14px] tracking-[-0.7px] uppercase">
@@ -144,10 +184,46 @@ const ProyectosDestacados = () => {
                 </p>
               </div>
             </div>
+            <div className="block md:hidden bg-secondary-000 rounded-[16px]  w-full  h-full px-[20px] py-[24px]">
+                <div className="flex items-center justify-between">
+                  <div className="] bg-p-orange-600 rounded-[6px] h-[26px] w-[70px] flex items-center justify-center">
+                    <span className="text-secondary-000 font-inter text-[14px] leading-[14px] tracking-[-0.7px] uppercase">
+                      Cliente
+                    </span>
+                  </div>
+                  <div className="w-[106px] h-auto">
+                    <img
+                      src="/img/clientes/pan-american.svg"
+                      alt=""
+                      className="w-[106px] h-auto"
+                    />
+                  </div>
+                </div>
+                <div className="flex items-center gap-[8px]">
+                  {/* <Chevron stroke="#4A5468" className="w-[6px] h-[14px]" /> */}
+                  <p className="text-secondary-700 font-inter font-semibold text-[16px] leading-[24px] tracking-[-0.64px]">
+                    Proyecto:
+                  </p>
+                </div>
+                <p className="text-p-blue-600 font-muli text-[14px] leading-[24px] tracking-[-0.56px] mb-[10px]">
+                  PO Planta Compresora
+                </p>
+                <div className="flex items-center gap-[8px]">
+                  {/* <Chevron stroke="#4A5468" className="w-[6px] h-[14px]" /> */}
+                  <p className="text-secondary-700 font-inter font-semibold text-[16px] leading-[24px] tracking-[-0.64px]">
+                    Alcance:
+                  </p>
+                </div>
+                <p className="text-p-blue-600 font-muli text-[14px] leading-[24px] tracking-[-0.56px]">
+                  Ingeniería Conceptual y Básica
+                </p>
+              </div>
+            </div>
           </SwiperSlide>
           <SwiperSlide>
-            <div className="w-full lg:w-[918px] h-[462px] bg-[url('/img/proyectos-destacados/03.jpg')] bg-cover bg-bottom rounded-[20px] lg:rounded-[40px] px-[20px] lg:px-[30px] py-[30px] lg:py-[40px] flex items-end justify-end">
-              <div className="px-[16px] lg:px-[20px] py-[20px] lg:py-[24px] bg-secondary-000 rounded-[16px] lg:rounded-[20px] w-full lg:w-[464px] h-auto">
+          <div className="flex flex-col w-full h-full mx-[20px] lg:mx-0 md:block md:mx-0">
+            <div className="w-full h-[280px] lg:mx-0 lg:w-[918px] md:h-[462px] bg-[url('/img/proyectos-destacados/03.jpg')] bg-cover bg-bottom rounded-[20px] lg:rounded-[40px] px-[20px] lg:px-[30px] py-[30px] lg:py-[40px] flex items-end justify-end">
+              <div className="hidden md:block px-[16px] lg:px-[20px] py-[20px] lg:py-[24px] bg-secondary-000 rounded-[16px] lg:rounded-[20px] w-full lg:w-[464px] h-auto">
                 <div className="flex items-center justify-between">
                   <div className="] bg-p-orange-600 rounded-[6px] h-[26px] w-[70px] flex items-center justify-center">
                     <span className="text-secondary-000 font-inter text-[14px] leading-[14px] tracking-[-0.7px] uppercase">
@@ -182,10 +258,46 @@ const ProyectosDestacados = () => {
                 </p>
               </div>
             </div>
+            <div className="block md:hidden bg-secondary-000 rounded-[16px]  w-full  h-full px-[20px] py-[24px]">
+                <div className="flex items-center justify-between">
+                  <div className="] bg-p-orange-600 rounded-[6px] h-[26px] w-[70px] flex items-center justify-center">
+                    <span className="text-secondary-000 font-inter text-[14px] leading-[14px] tracking-[-0.7px] uppercase">
+                      Cliente
+                    </span>
+                  </div>
+                  <div className="w-[106px] h-auto">
+                    <img
+                      src="/img/clientes/pan-american.svg"
+                      alt=""
+                      className="w-[106px] h-auto"
+                    />
+                  </div>
+                </div>
+                <div className="flex items-center gap-[8px]">
+                  {/* <Chevron stroke="#4A5468" className="w-[6px] h-[14px]" /> */}
+                  <p className="text-secondary-700 font-inter font-semibold text-[16px] leading-[24px] tracking-[-0.64px]">
+                    Proyecto:
+                  </p>
+                </div>
+                <p className="text-p-blue-600 font-muli text-[14px] leading-[24px] tracking-[-0.56px] mb-[10px]">
+                  PO Planta Compresora
+                </p>
+                <div className="flex items-center gap-[8px]">
+                  {/* <Chevron stroke="#4A5468" className="w-[6px] h-[14px]" /> */}
+                  <p className="text-secondary-700 font-inter font-semibold text-[16px] leading-[24px] tracking-[-0.64px]">
+                    Alcance:
+                  </p>
+                </div>
+                <p className="text-p-blue-600 font-muli text-[14px] leading-[24px] tracking-[-0.56px]">
+                  Ingeniería Conceptual y Básica
+                </p>
+              </div>
+            </div>
           </SwiperSlide>
           <SwiperSlide>
-            <div className="w-full lg:w-[918px] h-[462px] bg-[url('/img/proyectos-destacados/04.jpeg')] bg-cover bg-bottom rounded-[20px] lg:rounded-[40px] px-[20px] lg:px-[30px] py-[30px] lg:py-[40px] flex items-end justify-end">
-              <div className="px-[16px] lg:px-[20px] pt-[14px] pb-[20px] lg:pb-[24px] bg-secondary-000 rounded-[16px] lg:rounded-[20px] w-full lg:w-[464px] h-auto">
+          <div className="flex flex-col w-full h-full mx-[20px] lg:mx-0 md:block md:mx-0">
+            <div className="w-full h-[280px] lg:mx-0 lg:w-[918px] md:h-[462px] bg-[url('/img/proyectos-destacados/04.jpeg')] bg-cover bg-bottom rounded-[20px] lg:rounded-[40px] px-[20px] lg:px-[30px] py-[30px] lg:py-[40px] flex items-end justify-end">
+              <div className="hidden md:block px-[16px] lg:px-[20px] pt-[14px] pb-[20px] lg:pb-[24px] bg-secondary-000 rounded-[16px] lg:rounded-[20px] w-full lg:w-[464px] h-auto">
                 <div className="flex items-center justify-between">
                   <div className="] bg-p-orange-600 rounded-[6px] h-[26px] w-[70px] flex items-center justify-center">
                     <span className="text-secondary-000 font-inter text-[14px] leading-[14px] tracking-[-0.7px] uppercase">
@@ -222,25 +334,60 @@ const ProyectosDestacados = () => {
                 </p>
               </div>
             </div>
+            <div className="block md:hidden bg-secondary-000 rounded-[16px]  w-full  h-full px-[20px] py-[24px]">
+                <div className="flex items-center justify-between">
+                  <div className="] bg-p-orange-600 rounded-[6px] h-[26px] w-[70px] flex items-center justify-center">
+                    <span className="text-secondary-000 font-inter text-[14px] leading-[14px] tracking-[-0.7px] uppercase">
+                      Cliente
+                    </span>
+                  </div>
+                  <div className="w-[106px] h-auto">
+                    <img
+                      src="/img/clientes/pan-american.svg"
+                      alt=""
+                      className="w-[106px] h-auto"
+                    />
+                  </div>
+                </div>
+                <div className="flex items-center gap-[8px]">
+                  {/* <Chevron stroke="#4A5468" className="w-[6px] h-[14px]" /> */}
+                  <p className="text-secondary-700 font-inter font-semibold text-[16px] leading-[24px] tracking-[-0.64px]">
+                    Proyecto:
+                  </p>
+                </div>
+                <p className="text-p-blue-600 font-muli text-[14px] leading-[24px] tracking-[-0.56px] mb-[10px]">
+                  PO Planta Compresora
+                </p>
+                <div className="flex items-center gap-[8px]">
+                  {/* <Chevron stroke="#4A5468" className="w-[6px] h-[14px]" /> */}
+                  <p className="text-secondary-700 font-inter font-semibold text-[16px] leading-[24px] tracking-[-0.64px]">
+                    Alcance:
+                  </p>
+                </div>
+                <p className="text-p-blue-600 font-muli text-[14px] leading-[24px] tracking-[-0.56px]">
+                  Ingeniería Conceptual y Básica
+                </p>
+              </div>
+            </div>
           </SwiperSlide>
         </Swiper>
-        
+
         {/* Desktop Navigation - Hidden on mobile */}
         <button className="hidden lg:flex z-20 arrow-left arrow">
-          <Chevron stroke="#fff" className="rotate-180"/>
+          <Chevron stroke="#fff" className="rotate-180" />
         </button>
         <button className="hidden lg:flex z-20 arrow-right arrow">
-          <Chevron stroke="#fff"  />
+          <Chevron stroke="#fff" />
         </button>
-        
+
         {/* Mobile Navigation and Pagination - Hidden on desktop */}
         <div className="flex lg:hidden items-center justify-center gap-4 px-[20px] pb-[40px]">
           <button className="arrow-left-mobile arrow-mobile">
-            <Chevron stroke="#fff" className="rotate-180"/>
+            <Chevron stroke="#fff" className="rotate-180" />
           </button>
-          
+
           <div className="custom-pagination-proyectos bg-[#F7F7F7] p-[8px] rounded-[6px] flex gap-[8px] w-fit"></div>
-          
+
           <button className="arrow-right-mobile arrow-mobile">
             <Chevron stroke="#fff" />
           </button>
