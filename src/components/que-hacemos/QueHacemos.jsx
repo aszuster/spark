5d98f40c53780123eb1f4d4bd923dfa2ccff94cc
Alejandra <ale.szuster@gmail.com>
2026-07-28 15:04:06 -0300

    update que hacemos mobile

--- src/components/que-hacemos/QueHacemos.jsx
+++ src/components/que-hacemos/QueHacemos.jsx
@@ -25,74 +25,99 @@ const QueHacemos = () => {
       <div className="hidden lg:flex align-center w-full justify-between lg:max-w-[1080px] xl:max-w-[1280px] xxl:max-w-[1600px] lg:mx-auto py-[175px] gap-[150px]">
         <div className="basis-1/3">
           <img src="/img/que-hacemos/logo-orange-2.svg" className="w-[150px]" />
-          <p className="font-bold uppercase py-[15px]">Ingeniería y construcción</p>
+          <p className="font-bold uppercase py-[15px]">
+            Ingeniería y construcción
+          </p>
           <div className="flex flex-col gap-[20px] pt-[10px]">
-          <div>
-            <p className="text-p-orange-500 font-bold">Ingeniería [E]</p>
-            <p className="font-medium leading-[20px]">
-              Desarrollamos Ingeniería en todas sus fases. Ingeniería
-              Conceptual, Básica, y de Detalle. Estudios Especiales, Estimación
-              de Costos, y Evaluación de Proyectos.
-            </p>
-          </div>
-          <div>
-            <p className="text-p-orange-500 font-bold">Plantas y Equipos Modulares [EP]</p>
-            <p className="font-medium leading-[20px]">
-              Diseñamos y Fabricamos Plantas de Procesos y Equipos Modulares.
-            </p>
-          </div>
-          <div>
-            <p className="text-p-orange-500 font-bold">Plantas Llave en Mano [EPC]</p>
-            <p className="font-medium leading-[20px]">
-              Diseñamos, Fabricamos y Construimos Plantas de Procesos Llave en
-              Mano.
-            </p>
-          </div>
-          <div>
-            <p className="text-p-orange-500 font-bold">Gestión de Proyectos [EPCm / OE]</p>
-            <p className="font-medium leading-[20px]">
-              Realizamos la Gestión Integral de Proyectos. Ingeniería de
-              Propiedad, Gestión de Suministros, Gestión Contractual, Gestión de
-              Calidad, Oficina Técnica y Supervisión.
-            </p>
-          </div>
+            <div>
+              <p className="text-p-orange-500 font-bold">Ingeniería [E]</p>
+              <p className="font-medium leading-[20px]">
+                Desarrollamos Ingeniería en todas sus fases. Ingeniería
+                Conceptual, Básica, y de Detalle. Estudios Especiales,
+                Estimación de Costos, y Evaluación de Proyectos.
+              </p>
+            </div>
+            <div>
+              <p className="text-p-orange-500 font-bold">
+                Plantas y Equipos Modulares [EP]
+              </p>
+              <p className="font-medium leading-[20px]">
+                Diseñamos y Fabricamos Plantas de Procesos y Equipos Modulares.
+              </p>
+            </div>
+            <div>
+              <p className="text-p-orange-500 font-bold">
+                Plantas Llave en Mano [EPC]
+              </p>
+              <p className="font-medium leading-[20px]">
+                Diseñamos, Fabricamos y Construimos Plantas de Procesos Llave en
+                Mano.
+              </p>
+            </div>
+            <div>
+              <p className="text-p-orange-500 font-bold">
+                Gestión de Proyectos [EPCm / OE]
+              </p>
+              <p className="font-medium leading-[20px]">
+                Realizamos la Gestión Integral de Proyectos. Ingeniería de
+                Propiedad, Gestión de Suministros, Gestión Contractual, Gestión
+                de Calidad, Oficina Técnica y Supervisión.
+              </p>
+            </div>
           </div>
         </div>
         <div className="basis-1/3">
           <img src="/img/que-hacemos/logo-orange-2.svg" className="w-[150px]" />
-                    <p className="font-bold uppercase py-[15px]">Servicios petroleros</p>
+          <p className="font-bold uppercase py-[15px]">Servicios petroleros</p>
           <div className="flex flex-col gap-[20px] pt-[10px]">
-          <div>
-            <p className="text-p-orange-500 font-bold">Operación y Mantenimiento [O&M]</p>
-            <p className="font-medium leading-[20px]">
-              Realizamos Servicios de Operación de Plantas de Proceso para terceros. Soporte Técnico a Operaciones. Precomisionado, Comisionado, y Puesta en Marcha.
-            </p>
-          </div>
-          <div>
-            <p className="text-p-orange-500 font-bold">Well Testing</p>
-            <p className="font-medium leading-[20px]">
-             Servicio de medición y análisis de las variables asociadas a la producción de pozos.
-            </p>
-          </div>
-          <div>
-            <p className="text-p-orange-500 font-bold">Servicios de Procesamiento de Gas y Petróleo </p>
-            <p className="font-medium leading-[20px]">
-              Realizamos Servicios de Procesamientos de Gas y Petróleo para terceros con equipos propios.
-            </p>
-          </div>
+            <div>
+              <p className="text-p-orange-500 font-bold">
+                Operación y Mantenimiento [O&M]
+              </p>
+              <p className="font-medium leading-[20px]">
+                Realizamos Servicios de Operación de Plantas de Proceso para
+                terceros. Soporte Técnico a Operaciones. Precomisionado,
+                Comisionado, y Puesta en Marcha.
+              </p>
+            </div>
+            <div>
+              <p className="text-p-orange-500 font-bold">Well Testing</p>
+              <p className="font-medium leading-[20px]">
+                Servicio de medición y análisis de las variables asociadas a la
+                producción de pozos.
+              </p>
+            </div>
+            <div>
+              <p className="text-p-orange-500 font-bold">
+                Servicios de Procesamiento de Gas y Petróleo{" "}
+              </p>
+              <p className="font-medium leading-[20px]">
+                Realizamos Servicios de Procesamientos de Gas y Petróleo para
+                terceros con equipos propios.
+              </p>
+            </div>
           </div>
         </div>
         <div className="basis-1/3">
           <img src="/img/que-hacemos/logo-orange-2.svg" className="w-[150px]" />
-                    <p className="font-bold uppercase py-[15px]">Midstream</p>
+          <p className="font-bold uppercase py-[15px]">Midstream</p>
           <div className="flex flex-col gap-[20px] pt-[10px]">
-          <div>
-            <p className="text-p-orange-500 font-bold">Procesamiento y Comercialización de LGN y GLP</p>
-            <p className="font-medium leading-[20px]">
-              Procesamos corrientes de LGN (Líquidos del Gas Natural) y GLP (Gas Licuado de Petróleo) para su puesta en valor comercial y posterior comercialización.
-            </p>
-          </div>
-          <a href="https://fesa.com.ar/" className="px-[20px] py-[8px] rounded-3xl border border-black w-[155px] mt-[20px] hover:bg-p-orange-500 transition-all duration-300 hover:text-white hover:border-white"><p className="uppercase font-bold text-[18px] ">Midstream</p></a>
+            <div>
+              <p className="text-p-orange-500 font-bold">
+                Procesamiento y Comercialización de LGN y GLP
+              </p>
+              <p className="font-medium leading-[20px]">
+                Procesamos corrientes de LGN (Líquidos del Gas Natural) y GLP
+                (Gas Licuado de Petróleo) para su puesta en valor comercial y
+                posterior comercialización.
+              </p>
+            </div>
+            <a
+              href="https://fesa.com.ar/"
+              className="px-[20px] py-[8px] rounded-3xl border border-black w-[155px] mt-[20px] hover:bg-p-orange-500 transition-all duration-300 hover:text-white hover:border-white"
+            >
+              <p className="uppercase font-bold text-[18px] ">Midstream</p>
+            </a>
           </div>
         </div>
       </div>
@@ -132,96 +157,110 @@ const QueHacemos = () => {
               >
                 {/* Slide 1 — Ingeniería y construcción */}
                 <SwiperSlide>
-                  <div className="flex flex-col items-start gap-[20px]">
-                    <p className="text-[18px] font-muli uppercase pb-[4px]">
+                  <div className="">
+                    <p className="font-bold uppercase py-[15px]">
                       Ingeniería y construcción
                     </p>
-                    <div>
-                      <h3 className="text-p-orange-500 text-[16px] font-muli font-semibold">
-                        Ingeniería [E]
-                      </h3>
-                      <p className="font-inter text-[14px] leading-[18px] sl:leading-[24px]">
-                        Desarrollamos Ingeniería en todas sus fases. Ingeniería
-                        Conceptual, Básica, y de Detalle. Estudios Especiales,
-                        Estimación de Costos, y Evaluación de Proyectos.
-                      </p>
-                    </div>
-                    <div>
-                      <h3 className="text-p-orange-500 text-[16px] font-muli font-semibold">
-                        Plantas y Equipos Modulares [EP]
-                      </h3>
-                      <p className="font-inter text-[14px] leading-[18px] sl:leading-[24px]">
-                        Diseñamos y Fabricamos Plantas de Procesos y Equipos
-                        Modulares.
-                      </p>
-                    </div>
-                    <div>
-                      <h3 className="text-p-orange-500 text-[16px] font-muli font-semibold">
-                        Plantas Llave en Mano [EPC]
-                      </h3>
-                      <p className="font-inter text-[14px] leading-[18px] sl:leading-[24px]">
-                        Diseñamos, Fabricamos y Construimos Plantas de Procesos
-                        Llave en Mano.
-                      </p>
-                    </div>
-                    <div>
-                      <h3 className="text-p-orange-500 text-[16px] font-muli font-semibold">
-                        Gestión de Proyectos [EPCm / OE]
-                      </h3>
-                      <p className="font-inter text-[14px] leading-[18px] sl:leading-[24px]">
-                        Realizamos la Gestión Integral de Proyectos. Ingeniería
-                        de Propiedad, Gestión de Suministros, Gestión
-                        Contractual, Gestión de Calidad, Oficina Técnica y
-                        Supervisión.
-                      </p>
+                    <div className="flex flex-col gap-[20px] pt-[10px]">
+                      <div>
+                        <p className="text-p-orange-500 font-bold">
+                          Ingeniería [E]
+                        </p>
+                        <p className="font-medium leading-[20px]">
+                          Desarrollamos Ingeniería en todas sus fases.
+                          Ingeniería Conceptual, Básica, y de Detalle. Estudios
+                          Especiales, Estimación de Costos, y Evaluación de
+                          Proyectos.
+                        </p>
+                      </div>
+                      <div>
+                        <p className="text-p-orange-500 font-bold">
+                          Plantas y Equipos Modulares [EP]
+                        </p>
+                        <p className="font-medium leading-[20px]">
+                          Diseñamos y Fabricamos Plantas de Procesos y Equipos
+                          Modulares.
+                        </p>
+                      </div>
+                      <div>
+                        <p className="text-p-orange-500 font-bold">
+                          Plantas Llave en Mano [EPC]
+                        </p>
+                        <p className="font-medium leading-[20px]">
+                          Diseñamos, Fabricamos y Construimos Plantas de
+                          Procesos Llave en Mano.
+                        </p>
+                      </div>
+                      <div>
+                        <p className="text-p-orange-500 font-bold">
+                          Gestión de Proyectos [EPCm / OE]
+                        </p>
+                        <p className="font-medium leading-[20px]">
+                          Realizamos la Gestión Integral de Proyectos.
+                          Ingeniería de Propiedad, Gestión de Suministros,
+                          Gestión Contractual, Gestión de Calidad, Oficina
+                          Técnica y Supervisión.
+                        </p>
+                      </div>
                     </div>
                   </div>
                 </SwiperSlide>
                 {/* Slide 2 — Midstream */}
                 <SwiperSlide>
-                  <div className="flex flex-col items-start gap-[20px]">
-                    <p className="text-[18px] font-muli uppercase pb-[4px]">
-                      Midstream
-                    </p>
-                    <div>
-                      <h3 className="text-p-orange-500 text-[16px] font-muli font-semibold">
-                        Servicios de Procesamiento de Gas y Petróleo [BOOT]
-                      </h3>
-                      <p className="font-inter text-[14px] leading-[18px] sl:leading-[24px]">
-                        Realizamos Servicios de Procesamientos de Gas y Petróleo
-                        para terceros con equipos propios.
-                      </p>
-                    </div>
-                    <div>
-                      <h3 className="text-p-orange-500 text-[16px] font-muli font-semibold">
-                        Procesamiento y Comercialización de LGN y GLP
-                      </h3>
-                      <p className="font-inter text-[14px] leading-[18px] sl:leading-[24px]">
-                        Procesamos corrientes de LGN (Líquidos del Gas Natural)
-                        y GLP (Gas Licuado de Petróleo) para su puesta en valor
-                        comercial y posterior comercialización.
-                      </p>
-                    </div>
-                    <div>
-                      <h3 className="text-p-orange-500 text-[16px] font-muli font-semibold">
-                        Operación y Mantenimiento [O&M]
-                      </h3>
-                      <p className="font-inter text-[14px] leading-[18px] sl:leading-[24px]">
-                        Realizamos Servicios de Operación de Plantas de Proceso
-                        para terceros. Soporte Técnico a Operaciones.
-                        Precomisionado, Comisionado, y Puesta en Marcha.
-                      </p>
-                    </div>
-                    <div>
-                      <h3 className="text-p-orange-500 text-[16px] font-muli font-semibold">
-                        Well Testing [WT]
-                      </h3>
-                      <p className="font-inter text-[14px] leading-[18px] sl:leading-[24px]">
-                        Servicio de medición y análisis de las variables
-                        asociadas a la producción de pozos.
-                      </p>
-                    </div>
-                  </div>
+        <div className="">
+          <p className="font-bold uppercase py-[15px]">Servicios petroleros</p>
+          <div className="flex flex-col gap-[20px] pt-[10px]">
+            <div>
+              <p className="text-p-orange-500 font-bold">
+                Operación y Mantenimiento [O&M]
+              </p>
+              <p className="font-medium leading-[20px]">
+                Realizamos Servicios de Operación de Plantas de Proceso para
+                terceros. Soporte Técnico a Operaciones. Precomisionado,
+                Comisionado, y Puesta en Marcha.
+              </p>
+            </div>
+            <div>
+              <p className="text-p-orange-500 font-bold">Well Testing</p>
+              <p className="font-medium leading-[20px]">
+                Servicio de medición y análisis de las variables asociadas a la
+                producción de pozos.
+              </p>
+            </div>
+            <div>
+              <p className="text-p-orange-500 font-bold">
+                Servicios de Procesamiento de Gas y Petróleo{" "}
+              </p>
+              <p className="font-medium leading-[20px]">
+                Realizamos Servicios de Procesamientos de Gas y Petróleo para
+                terceros con equipos propios.
+              </p>
+            </div>
+          </div>
+        </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                          <div className="">
+          <p className="font-bold uppercase py-[15px]">Midstream</p>
+          <div className="flex flex-col gap-[20px] pt-[10px]">
+            <div>
+              <p className="text-p-orange-500 font-bold">
+                Procesamiento y Comercialización de LGN y GLP
+              </p>
+              <p className="font-medium leading-[20px]">
+                Procesamos corrientes de LGN (Líquidos del Gas Natural) y GLP
+                (Gas Licuado de Petróleo) para su puesta en valor comercial y
+                posterior comercialización.
+              </p>
+            </div>
+            <a
+              href="https://fesa.com.ar/"
+              className="px-[20px] py-[8px] rounded-3xl border border-black w-[155px] mt-[20px] hover:bg-p-orange-500 transition-all duration-300 hover:text-white hover:border-white"
+            >
+              <p className="uppercase font-bold text-[18px] ">Midstream</p>
+            </a>
+          </div>
+        </div>
                 </SwiperSlide>
               </Swiper>
 
