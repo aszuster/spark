87fe470b3c626ac97aa1fec86b032c66564d43b7
Alejandra <ale.szuster@gmail.com>
2026-08-03 17:04:17 -0300

    fix for hero video filename

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -92,7 +92,7 @@ const Hero = ({ onAnimationComplete }) => {
             width="100%"
             height="auto"
           >
-            <source src="/video/hero.mp4" type="video/mp4" />
+            <source src="/video/hero-video.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </motion.video>
 
